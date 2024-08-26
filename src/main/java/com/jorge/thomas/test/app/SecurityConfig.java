02449490b098f540eef75bc8abf6eed6720c3bdf
Jorge Narvaez <jormelcn@gmail.com>
2024-08-25 23:05:04 -0500

    Update security config

--- src/main/java/com/jorge/thomas/test/app/SecurityConfig.java
+++ src/main/java/com/jorge/thomas/test/app/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.jorge.thomas.test.app;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,9 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import com.jorge.thomas.test.app.auth.reqfilters.JWTRequestFilter;
@@ -62,9 +57,10 @@ public class SecurityConfig {
     http
         .csrf(x -> x.disable())
         .authorizeHttpRequests(x -> x
+            .requestMatchers("/error").permitAll()
             .requestMatchers("/auth/**").permitAll()
             .requestMatchers("/swagger-ui/**").permitAll()
-            .requestMatchers("v3/api-docs/**").permitAll()
+            .requestMatchers("/v3/api-docs/**").permitAll()
             .anyRequest().authenticated())
         .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authenticationProvider)
@@ -74,16 +70,5 @@ public class SecurityConfig {
                 handlerExceptionResolver),
             UsernamePasswordAuthenticationFilter.class);
     return http.build();
-  }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:8005"));
-    configuration.setAllowedMethods(List.of("GET", "POST"));
-    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
   }
 }
